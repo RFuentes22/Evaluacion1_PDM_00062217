f0661c9e8920325d6cdbeb1911684b0b199e3551
RFuentes22 <00062217@uca.edu.sv>
2019-05-08 12:37:02 -0600

    Working

---
+++ app/src/main/java/com/example/evaluation1/MainActivity.kt
@@ -0,0 +1,54 @@
+package com.example.evaluation1;
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.constraint.ConstraintLayout
+import android.support.v7.app.AppCompatActivity
+import android.widget.EditText
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity() {
+
+    fun refreshCount(v: TextView) { v.text = addOne(v) }
+
+    fun addOne(v: TextView): String =  (getInt(v) + 1).toString()
+
+    fun getInt(v: TextView): Int = if(isHashtag(v)) 0 else v.text.toString().toInt()
+
+    fun isHashtag(v: TextView): Boolean = v.text.toString() == "#"
+
+    fun fetchEditText(v: EditText): String = if (v.text.isEmpty()) "N/A" else v.text.toString()
+
+    fun getTotal(): Int = (getInt(product1cont)+getInt(product2cont)+getInt(product3cont)+getInt(product4cont)+getInt(product5cont)+getInt(product6cont)+getInt(product7cont)+getInt(product8cont)+getInt(product9cont))
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val Layouts: ArrayList<ConstraintLayout> = arrayListOf(lproduct1, lproduct2, lproduct3, lproduct4, lproduct5, lproduct6, lproduct7, lproduct8, lproduct9)
+        val Textviews: ArrayList<TextView> = arrayListOf(product1cont, product2cont, product3cont, product4cont, product5cont, product6cont, product7cont, product8cont, product9cont)
+
+        Layouts.forEachIndexed { index, layout -> layout.setOnClickListener { refreshCount(Textviews[index]) } }
+
+        sendbutton.setOnClickListener {
+
+            val senderjson: String = """
+            |{
+            |"username":"${fetchEditText(eusuario)}",
+            |"email":"${fetchEditText(ecorreo)}",
+            |"total":"${getTotal()}",
+            |"products":["${getInt(product1cont)}", "${getInt(product2cont)}", "${getInt(product3cont)}", "${getInt(product4cont)}", "${getInt(product5cont)}", "${getInt(product6cont)}", "${getInt(product7cont)}", "${getInt(product8cont)}", "${getInt(product9cont)}"]
+            |}
+            """.trimMargin()
+
+            val sendIntent = Intent(this, ViewerActivity::class.java)
+            sendIntent.putExtra("json", senderjson)
+            sendIntent.type = "json/plain"
+            startActivity(sendIntent)
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/evaluation1/ViewerActivity.kt
@@ -0,0 +1,58 @@
+package com.example.evaluation1
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_viewer.*
+import org.json.JSONObject
+
+
+class ViewerActivity : AppCompatActivity() {
+
+    fun identifyIntent(type: String): String = if (type == "json/plain") "json" else Intent.EXTRA_TEXT
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_viewer)
+
+        val recieverBundle = intent
+        val Textviews: ArrayList<TextView> = arrayListOf(vproduct1cont, vproduct2cont, vproduct3cont, vproduct4cont, vproduct5cont, vproduct6cont, vproduct7cont, vproduct8cont, vproduct9cont)
+        val jsonroot = JSONObject(recieverBundle.getStringExtra(identifyIntent(recieverBundle.type)))
+        val jsonproducts = jsonroot.getJSONArray("products")
+
+        vusuario.text = jsonroot.getString("username")
+        vcorreo.text = jsonroot.getString("email")
+        vtotal.text = jsonroot.getString("total")
+
+        for ((index, cont) in Textviews.withIndex()){
+            cont.text = jsonproducts[index].toString()
+        }
+
+        sharebutton.setOnClickListener {
+            val factura =
+                    """FACTURA DE COMPRA
+                        |
+                        |Cliente: ${vusuario.text}
+                        |Correo: ${vcorreo.text}
+                        |Producto 1: ${vproduct1cont.text}
+                        |Producto 2: ${vproduct2cont.text}
+                        |Producto 3: ${vproduct3cont.text}
+                        |Producto 4: ${vproduct4cont.text}
+                        |Producto 5: ${vproduct5cont.text}
+                        |Producto 6: ${vproduct6cont.text}
+                        |Producto 7: ${vproduct7cont.text}
+                        |Producto 8: ${vproduct8cont.text}
+                        |Producto 9: ${vproduct9cont.text}
+                        |
+                        |Total de productos: ${vtotal.text}
+                    """.trimMargin()
+            val shareIntent = Intent()
+            shareIntent.action = Intent.ACTION_SEND
+            shareIntent.putExtra(Intent.EXTRA_TEXT, factura)
+            shareIntent.type = "text/plain"
+            if (shareIntent.resolveActivity(packageManager) != null) startActivity(shareIntent)
+        }
+    }
+
+}
